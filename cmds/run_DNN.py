3a14530a727a1a38c173e50bcedc8901106db126
Ubuntu <ubuntu@ip-172-31-13-119.ec2.internal>
2016-04-19 18:41:15 +0000

    Data

--- cmds/run_DNN.py
+++ cmds/run_DNN.py
@@ -80,6 +80,8 @@ if __name__ == '__main__':
     if 'ptr_file' in arguments and 'ptr_layer_number' in arguments:
         ptr_file = arguments['ptr_file']
         ptr_layer_number = int(arguments['ptr_layer_number'])
+ 
+    log('Ptr-file '+ptr_file+' ptr layer='+str(ptr_layer_number));
 
     # check working dir to see whether it's resuming training
     resume_training = False
@@ -100,8 +102,8 @@ if __name__ == '__main__':
     # initialize model parameters
     # if not resuming training, initialized from the specified pre-training file
     # if resuming training, initialized from the tmp model file
-    if (ptr_layer_number > 0) and (resume_training is False):
-        log('>... Loading retrained data from '+str(ptr_file))
+    if (ptr_layer_number != 0) and (resume_training is False):
+        log('>... Loading pretrained data from '+str(ptr_file))
         _file2nnet(dnn.layers, set_layer_num = ptr_layer_number, path = ptr_file)
     if resume_training:
         _file2nnet(dnn.layers, path = wdir + '/dnn.tmp')

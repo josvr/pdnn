9dd127de0246eea98d7119f6857bfb01d647b25d
josr <jos.v.roosmalen@gmail.com>
2016-04-22 12:18:58 +0200

    Signal handler improvements

--- utils/stop_handler.py
+++ utils/stop_handler.py
@@ -1,17 +1,22 @@
 # Globals
 import signal
+from io_func.model_io import log
 
 stop_requested = False
 init = False
 
 def init_signal_handler():
    def handler(x,y):
+       log("SIGUSR1 SIGNAL RECEIVED")
        global stopRequested 
        stopRequested = True
-
+         
+   log("SETUP SIGNAL HANDLER SIGUSR1")
    signal.signal(signal.SIGUSR1, handler)
+   init = True
 
 def stop_if_stop_is_requested(): 
+   global stop_requested
    if init == False:
     init_signal_handler()
    if stop_requested: 
